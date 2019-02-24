471bd37071f58742fab831365aa87a48fe6b552e
lebo <lebo@bo.bo>
2019-02-24 09:47:58 +0700

    20190224

---
+++ DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,120 @@
+return {
+  mod_publish_data_file={ configuration_options={  }, enabled=true },
+  ["mod_publish_data_file (1)"]={
+    configuration_options={
+      EVERGREEN_CHOPS_NORMAL=10,
+      EVERGREEN_CHOPS_SMALL=5,
+      EVERGREEN_CHOPS_TALL=15,
+      ICE_MINE=3,
+      MARBLEPILLAR_MINE=10,
+      MARBLETREE_MINE=8 
+    },
+    enabled=true 
+  },
+  ["workshop-1185229307"]={ configuration_options={  }, enabled=true },
+  ["workshop-1207269058"]={
+    configuration_options={
+      ddon=true,
+      hbcolor="dynamic",
+      hblength=10,
+      hbpos=1,
+      hbstyle="heart",
+      value=true 
+    },
+    enabled=true 
+  },
+  ["workshop-1250327920"]={ configuration_options={ active=true }, enabled=true },
+  ["workshop-1422039508"]={
+    configuration_options={
+      [""]=0,
+      CropGrowthTime=1,
+      CropWitherTime=240,
+      CropWitherability=1,
+      FoodSettings=0,
+      NumberofCropsPerHarvest=1,
+      NumberofHarvests=0,
+      SeedPacketRecipe=1,
+      SeedPacketRecipeDifficulty=3,
+      SoilCycles=1 
+    },
+    enabled=true 
+  },
+  ["workshop-1449336053"]={ configuration_options={  }, enabled=true },
+  ["workshop-1535658505"]={ configuration_options={ RECIPE=3 }, enabled=true },
+  ["workshop-1547161641"]={ configuration_options={ GLZ="false" }, enabled=true },
+  ["workshop-1604299284"]={ configuration_options={ auto_stack=true }, enabled=true },
+  ["workshop-356930882"]={ configuration_options={ uses=10000000, uses2=10000000 }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-379707873"]={
+    configuration_options={
+      BeeArena="normal",
+      BrokenPseudoscienceStation="normal",
+      ClockworkArena="normal",
+      MermArena="normal",
+      PigtorchArena="normal",
+      SpiderArena="normal",
+      TentacleArena="normal" 
+    },
+    enabled=true 
+  },
+  ["workshop-447092740"]={ configuration_options={  }, enabled=true },
+  ["workshop-925172054"]={ configuration_options={ TAB=1, ganjabush_rate=1 }, enabled=true },
+  ["workshop-942328680"]={
+    configuration_options={
+      [""]=0,
+      amulet=true,
+      armorsnurtleshell=true,
+      attack=true,
+      attacked=true,
+      batcave=true,
+      batwing=true,
+      berries=true,
+      berries_juicy=true,
+      campfire=true,
+      cave_banana=true,
+      cave_banana_cooked=true,
+      coontail=true,
+      cutgrass=true,
+      cutreeds=true,
+      deerclops_eyeball=true,
+      dig=true,
+      fertilize=true,
+      fireflies=true,
+      firepit=true,
+      fish=true,
+      harvest=true,
+      houndstooth=true,
+      hungry_attack=true,
+      level_up_rate=1.5,
+      lightbulb=true,
+      livinglog=true,
+      molehill=true,
+      penalty_divide=2,
+      pigskin=true,
+      pitchfork=true,
+      pond=true,
+      self_cannibalism=true,
+      shovel=true,
+      skeleton_player=true,
+      start_message=2,
+      tallbirdegg=true,
+      twigs=true,
+      walrus_camp=true,
+      walrus_tusk=true 
+    },
+    enabled=true 
+  } 
+}
---
+++ DoNotStarveTogether/dedicated_server_mods_setup.lua
@@ -0,0 +1,34 @@
+ServerModSetup("343753877")
+ServerModSetup("351325790")
+ServerModSetup("356930882")
+ServerModSetup("362175979")
+--ServerModSetup("365119238")
+ServerModSetup("375850593")
+ServerModSetup("376333686")
+ServerModSetup("378160973")
+--ServerModSetup("379707873")
+ServerModSetup("447092740")
+ServerModSetup("501385076") --- Quick Pick
+--ServerModSetup("785009843") --QP2
+ServerModSetup("599498678")
+ServerModSetup("694531911")
+ServerModSetup("727774324")
+ServerModSetup("925172054")
+--ServerModSetup("942328680")
+--ServerModSetup("958030355")
+ServerModSetup("1250327920")
+--ServerModSetup("1295277999")
+ServerModSetup("1299647282")
+--ServerModSetup("1417656559")
+--ServerModSetup("1422039508")
+ServerModSetup("1474293089")
+ServerModSetup("1535658505")
+--ServerModSetup("1547161641")
+ServerModSetup("1207269058") --Health bar
+ServerModSetup("1458450094") --Auto Trap Reset
+--ServerModSetup("1484294726")
+--ServerModSetup("1505270912")
+--ServerModSetup("1628298090") --G10MM-3R
+--ServerModSetup("375859599") --Health info
+ServerModSetup("1547161641") --Hmlet stuff
+
